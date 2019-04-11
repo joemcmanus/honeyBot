3a6fc460dd4adf2ffc401852e15eb5617dd1aac5
Joe McManus <joe.mcmanus@colorado.edu>
2019-04-11 15:28:59 -0600

    Updated with GPL

--- honeyMqtt.py
+++ honeyMqtt.py
@@ -1,8 +1,21 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # File    : honeyMqtt.py - Program to listen for MQTT events
 # Author  : Joe McManus josephmc@alumni.cmu.edu
 # Version : 0.1 04/01/2019
 # Copyright (C) 2019 Joe McManus
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import argparse
 import paho.mqtt.client as paho
